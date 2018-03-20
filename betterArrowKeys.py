d5cf6065f2c9045c9d4c151c4f5ba5a3b25c2bee
tatkon <tatkon@rbtrainers.com>
2018-03-20 15:03:49 -0500

    more whitespace issues

--- betterArrowKeys.py
+++ betterArrowKeys.py
@@ -9,13 +9,13 @@ while 1:
 	x1 = y1 = 0
 	if keyboard.KeyboardEvent: #maybe?
 		if keyboard.is_pressed('w'):
-	        y1 = 1
-       	elif keyboard.is_pressed('s'):
-            y1 = -1
-        if keyboard.is_pressed('a'):
-            x1 = -1
-        elif keyboard.is_pressed('d'):
-            x1 = 1
+		        y1 = 1
+       		elif keyboard.is_pressed('s'):
+            		y1 = -1
+        	if keyboard.is_pressed('a'):
+            		x1 = -1
+        	elif keyboard.is_pressed('d'):
+            		x1 = 1
 		if keyboard.is_pressed('space'):
 			exit()
 	mower.drive(0, y1, x1)
