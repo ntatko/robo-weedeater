a287d56877588709ff85578f699fecd85cc98025
Noah Tatko <ntat.taco@gmail.com>
2018-03-20 14:48:20 -0500

    switched (temporarily) to wasd configuration

--- betterArrowKeys.py
+++ betterArrowKeys.py
@@ -11,15 +11,15 @@ while 1:
 		#drive(0,0,0,0,motor1,motor2,motor3) 	#break
 	x1 = y1 = 0
 	if keyboard.KeyboardEvent: #maybe?
-		if keyboard.is_pressed('up'):
+		if keyboard.is_pressed('w'):
 	        	y1 = 1
-       	elif keyboard.is_pressed('down'):
+       	elif keyboard.is_pressed('s'):
                 y1 = -1
-        elif keyboard.is_pressed('left'):
+        elif keyboard.is_pressed('a'):
                 x1 = -1
-        elif keyboard.is_pressed('right'):
+        elif keyboard.is_pressed('d'):
                 x1 = 1
-		elif keyboard.is_pressed('esc'):
+		elif keyboard.is_pressed('space'):
 			exit()
 	mower.drive(0, y1, x1)
 		#k=inkey()
