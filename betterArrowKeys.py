71ceff4a60b3d467a6181e89beffc889ca79d611
Noah Tatko <ntat.taco@gmail.com>
2018-03-20 14:54:51 -0500

    Fix whitespace error

--- betterArrowKeys.py
+++ betterArrowKeys.py
@@ -3,25 +3,20 @@ import keyboard
 
 mower = Mower()
 atexit.register(mower.turnOffMotors())
-       # inkey = _Getch()
-        #while(1):
-	#k=inkey()
+
 keyboard.start_recording()
 while 1:
-		#drive(0,0,0,0,motor1,motor2,motor3) 	#break
 	x1 = y1 = 0
 	if keyboard.KeyboardEvent: #maybe?
 		if keyboard.is_pressed('w'):
-	        	y1 = 1
+	        y1 = 1
        	elif keyboard.is_pressed('s'):
-                y1 = -1
-        elif keyboard.is_pressed('a'):
-                x1 = -1
+            y1 = -1
+        if keyboard.is_pressed('a'):
+            x1 = -1
         elif keyboard.is_pressed('d'):
-                x1 = 1
-		elif keyboard.is_pressed('space'):
+            x1 = 1
+		if keyboard.is_pressed('space'):
 			exit()
 	mower.drive(0, y1, x1)
-		#k=inkey()
-#drive(0,0,0,0,motor1,motor2,motor3
 	time.sleep(0.01)
