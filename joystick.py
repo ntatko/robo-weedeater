0541a1fa41f446810c4596b38d498c84ea85edb2
Noah Tatko <ntat.taco@gmail.com>
2018-03-21 22:14:52 -0500

    minor fixes

--- joystick.py
+++ joystick.py
@@ -11,12 +11,10 @@ while 1:
 	if controller.get_B():
 		mower.driveFast()
 	if controller.get_start() and mower.getCutter() == 0:
-		while controller.get_start():
-			time.sleep(0.01)
+		time.sleep(0.2)
 		mower.spinCutter()
 	elif controller.get_start() and mower.getCutter() == 1:
-		while controller.get_start():
-			time.sleep(0.01)
+		time.sleep(0.2)
 		mower.killCutter()
 	controller.get_X()
 	mower.drive(controller.get_leftX(), controller.get_leftY(), controller.get_rightX())
