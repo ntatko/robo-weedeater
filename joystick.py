5d3aa830ddb6d6f489d647c616e04c8c2ac14cb3
tatkon <tatkon@rbtrainers.com>
2018-03-24 17:07:39 -0500

    controller class testing

--- joystick.py
+++ joystick.py
@@ -16,6 +16,5 @@ while 1:
 	elif controller.get_start() and mower.getCutter() == 1:
 		time.sleep(0.2)
 		mower.killCutter()
-	controller.listen_for_hats()
 	mower.drive(controller.get_leftX(), controller.get_leftY(), controller.get_rightX())
 	time.sleep(0.01)
