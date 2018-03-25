c91e9880a484da43e84097b195e15b77317b5bac
= <=>
2018-03-25 04:46:18 -0500

    attempting problem solving...

--- sensorClass.py
+++ sensorClass.py
@@ -90,8 +90,10 @@ class Sensors:
         return [self._lastAccelerometer[0]-self._thisAccelerometer[0], self._lastAccelerometer[1]-self._thisAccelerometer[1], self._lastAccelerometer[2]-self._thisAccelerometer[2]]
 
     def get_compassHeading(self):
-        while self._originCompass == [] or self._thisCompass == []:
+        while self._thisCompass == []:
             self.refreshData()
+            print("refreshing data...")
+        print("returning angle")
         return arcsin((self._originCompass[0]*self._thisCompass[0] + self._originCompass[1]*self._thisCompass[1] +
                 self._originCompass[2]*self._thisCompass[2])/(((self._originCompass[0]**2 +
                 self._originCompass[1]**2 + self._originCompass[2]**2)**0.5) * (self._thisCompass[0]**2 +
