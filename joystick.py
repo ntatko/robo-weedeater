cf8192f2f5f073fdd095c2c95ba07b117718fee9
= <=>
2018-03-26 12:46:24 -0500

    Sensor class wasn't working out for me, although the concept was good

--- joystick.py
+++ joystick.py
@@ -18,7 +18,5 @@ while 1:
 	elif controller.get_start() and mower.getCutter() == 1:
 		time.sleep(0.2)
 		mower.killCutter()
-	sensors.refreshData()
-	print(sensors.get_compassHeading())
 	mower.drive(controller.get_leftX(), controller.get_leftY(), controller.get_rightX())
 	time.sleep(0.01)
